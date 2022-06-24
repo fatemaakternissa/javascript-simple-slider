93c74107e7284900695dfb1fd3c40ad351c2b6b9
fatemaakternissa <fatemaakternissa3gmail.com>
2022-06-24 16:06:56 +0600

    javascript slider code added

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-# javascript-simple-slider
+// javascript simple slider source code
+
+// variables
+const slider = document.querySelector(".slider");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+let sectionindex = 0;
+
+// prevBtn
+prevBtn.addEventListener("click", function(){
+    sectionindex = sectionindex > 0 ? sectionindex - 1 : sectionindex;
+    slider.style.transform = "translate("+ (sectionindex) * - 25 +"%)"; 
+   
+});
+
+// nextBtn 
+nextBtn.addEventListener("click", function(){
+    sectionindex = sectionindex < 3 ? sectionindex + 1 : sectionindex;
+    slider.style.transform = "translate("+ (sectionindex) * - 25 +"%)"; 
+   
+});
